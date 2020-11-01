169fa017b891137967f8d173c975f6c9fb0a9147
Sergey Bashkin <bashkin2013@mail.ru>
2020-11-01 13:03:26 +0400

    feat: added header in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from './components/Header';
 
-function App() {
+const AppBody = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 1000px;
+`;
+
+const App = () => {
   return (
-    <></>
+    <AppBody>
+      <Header />
+    </AppBody>
   );
 }
 
