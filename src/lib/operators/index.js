2c7f87699ac5f8a16c68135d7f969569a1272845
Sergey Bashkin <bashkin2013@mail.ru>
2020-11-01 12:35:39 +0400

    feat: added array of operators

---
+++ src/lib/operators/index.js
@@ -0,0 +1,23 @@
+import mts from '../../assets/mts.png';
+import beeline from '../../assets/beeline.png';
+import megafon from '../../assets/megafon.png';
+
+const operators = [{
+    id: 1,
+    name: 'МТС',
+    url: 'mts',
+    photo: mts,
+  }, {
+    id: 2,
+    name: 'Билайн',
+    url: 'beeline',
+    photo: beeline,
+  }, {
+    id: 3,
+    name: 'Мегафон',
+    url: 'megafon',
+    photo: megafon,
+  }
+]
+
+export default operators;
