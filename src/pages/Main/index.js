d997c87c9a035791fd0dc38281e2f9ad591ac95c
Sergey Bashkin <bashkin2013@mail.ru>
2020-11-01 20:22:30 +0400

    feat: added new styles for card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,26 +3,31 @@ import { Link } from 'react-router-dom';
 import operators from '../../lib/operators';
 import * as Styles from './style';
 
-const Main = () => {
-  return (
-    <>
-      <Styles.MainTitle>Выберите оператора</Styles.MainTitle>
+const Main = () => (
+  <>
+    <Styles.MainTitle>Выберите оператора</Styles.MainTitle>
+    <Styles.OperatorsList>
       {
         operators.map((operator) => (
           <Link 
             to={`/pay/${operator.url}`}
             key={operator.id}
           >
-            <img
-              src={operator.photo}
-              alt={operator.name}
-            />
-            <p>{operator.name}</p>
+            <Styles.OpeatorCard
+              firstColor={operator.firstColor}
+              secondColor={operator.secondColor}
+            >
+              <Styles.OperatorImage
+                src={operator.photo}
+                alt={operator.name}
+              />
+              <Styles.OperatorName>{operator.name}</Styles.OperatorName>
+            </Styles.OpeatorCard>
           </Link>
         ))
       }
-    </>
-  )
-}
+    </Styles.OperatorsList>
+  </>
+);
 
 export default Main;
--- src/pages/Main/style.js
+++ src/pages/Main/style.js
@@ -4,4 +4,36 @@ const MainTitle = styled.h2`
   font-size: 2.4rem;
 `;
 
-export { MainTitle};
+const OperatorsList = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-row-gap: 30px;
+`;
+
+const OpeatorCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 85%;
+  margin: 0 auto;
+  padding: 20px;
+  background: linear-gradient(
+    90deg, 
+    ${props => props.firstColor}, 
+    ${props => props.secondColor});
+  border-radius: 20px;
+  box-shadow: 0 6px 18px 0 rgba(31,34,41,.18);
+`;
+
+const OperatorImage = styled.img`
+  width: 70px;
+  height: 70px;
+  border-radius: 100%;
+`;
+
+const OperatorName = styled.p`
+  font-size: 1.6rem;
+  color: white;
+  margin-bottom: 10px;
+`;
+
+export { MainTitle, OperatorsList, OpeatorCard, OperatorImage, OperatorName };
