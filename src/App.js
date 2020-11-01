4aec70bd251b7f28ee385737ad6633a9a942ffe2
Sergey Bashkin <bashkin2013@mail.ru>
2020-11-01 18:49:23 +0400

    feat: created routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Header from './components/Header';
 import GlobalStyles from './lib/globalStyles';
 
 const AppBody = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   width: 1000px;
+  margin: 0 auto;
 `;
 
 const App = () => {
   return (
-    <AppBody>
+    <Router>
       <GlobalStyles />
-      <Header />
-    </AppBody>
+      <AppBody>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <p>Первый</p>
+            <Link to="/ab">Переход</Link>
+          </Route>
+          <Route path="/ab">
+            <p>Второй</p>
+          </Route>
+        </Switch>
+      </AppBody>
+    </Router>
   );
 }
 
