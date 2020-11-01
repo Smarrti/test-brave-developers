d9bf66cc2eb90ed8b47215d297af152b48c1dd34
Sergey Bashkin <bashkin2013@mail.ru>
2020-11-01 13:46:06 +0400

    init: added global style rules

---
+++ src/lib/globalStyles/index.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    font-family: 'Montserrat', sans-serif;
+  };
+  body {
+    font-size: 10px;
+  }
+`;
+
+export default GlobalStyles;
