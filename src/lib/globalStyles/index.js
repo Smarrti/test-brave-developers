05fa7d82d0cd575f112719186ef736705767ce04
Sergey Bashkin <bashkin2013@mail.ru>
2020-11-01 19:48:54 +0400

    feat: added styes for a

--- src/lib/globalStyles/index.js
+++ src/lib/globalStyles/index.js
@@ -4,8 +4,13 @@ const GlobalStyles = createGlobalStyle`
   * {
     font-family: 'Montserrat', sans-serif;
   };
+
   body {
     font-size: 10px;
+  }
+
+  a {
+    text-decoration: none;
   }
 `;
 
