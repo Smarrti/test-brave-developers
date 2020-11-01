5ab8f7f3b4487b9940492aa62ec15b1aa385e53f
Sergey Bashkin <bashkin2013@mail.ru>
2020-11-01 20:06:04 +0400

    feat: added color of operators

--- src/lib/operators/index.js
+++ src/lib/operators/index.js
@@ -1,22 +1,28 @@
-import mts from '../../assets/mts.png';
-import beeline from '../../assets/beeline.png';
-import megafon from '../../assets/megafon.png';
+import mts from '../../assets/mts.jpg';
+import beeline from '../../assets/beeline.jpg';
+import megafon from '../../assets/megafon.jpg';
 
 const operators = [{
     id: 1,
     name: 'МТС',
     url: 'mts',
     photo: mts,
+    firstColor: '#e30510',
+    secondColor: '#c7040e',
   }, {
     id: 2,
     name: 'Билайн',
     url: 'beeline',
     photo: beeline,
+    firstColor: '#ffdb01',
+    secondColor: '#f2d000'
   }, {
     id: 3,
     name: 'Мегафон',
     url: 'megafon',
     photo: megafon,
+    firstColor: '#00b856',
+    secondColor: '#00a84f'
   }
 ]
 
